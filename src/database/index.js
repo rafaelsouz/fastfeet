66995038a6c6df7ae2a43e811b929eb570946d82
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-07 14:00:19 -0300

    loader de models

---
+++ src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => {
+      return model.init(this.connection);
+    });
+  }
+}
+
+export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ oi: 'eae' });
+routes.get('/', async (req, res) => {
+  return res.json({ oi: 'oi' });
 });
 
 export default routes;
