ddf82a37bd7864991e7365dd8cc338ed4f06ec0f
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-17 19:24:24 -0300

    Acresentando models de file e deliverman

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,12 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import Deliveryman from '../app/models/Deliveryman';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient];
+const models = [User, Recipient, Deliveryman, File];
 
 class Database {
   constructor() {
