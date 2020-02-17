8bffaff476b1b337b29f251db5135e02a66db006
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-17 20:34:53 -0300

    Adicionando metodo associate com model file

--- src/app/models/Deliveryman.js
+++ src/app/models/Deliveryman.js
@@ -13,6 +13,10 @@ class Deliveryman extends Model {
     );
     return this;
   }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
 }
 
 export default Deliveryman;
