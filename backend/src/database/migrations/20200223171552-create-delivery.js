c9245592658206d49fb8766b71cf7eb997d6df3a
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-24 12:41:47 -0300

    criando tabela delivery no banco

--- backend/src/database/migrations/20200223171552-create-delivery.js
+++ backend/src/database/migrations/20200223171552-create-delivery.js
@@ -14,6 +14,44 @@ module.exports = {
         onDelete: 'SET NULL',
         allowNull: false,
       },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'deliverymans', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false,
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'files', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: false,
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
