3ee5580be03e7bcfbb57487aa5fae94eca67e97e
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-24 18:34:32 -0300

    Cadastro de entregas

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -1,4 +1,9 @@
+import * as Yup from 'yup';
+
 import Delivery from '../models/Delivery';
+import User from '../models/User';
+import Recipient from '../models/Recipient';
+import Deliveryman from '../models/Deliveryman';
 
 class DeliveryController {
   async index(req, res) {
@@ -6,7 +11,43 @@ class DeliveryController {
   }
 
   async store(req, res) {
-    return res.json({});
+    const schema = Yup.object().shape({
+      recipient_id: Yup.number().required(),
+      deliveryman_id: Yup.number().required(),
+      product: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
+    const user = await User.findByPk(req.userId);
+
+    if (user.admin === 0) {
+      return res.status(401).json({ error: 'You are not allowed to do this' });
+    }
+
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const checkIsRecipient = await Recipient.findOne({
+      where: { recipient_id },
+    });
+
+    if (!checkIsRecipient) {
+      return res.status(401).json({ error: 'Recipient not found' });
+    }
+
+    const checkIsDeliveryman = await Deliveryman.findOne({
+      where: { deliveryman_id },
+    });
+
+    if (!checkIsDeliveryman) {
+      return res.status(401).json({ error: 'Deliveryman not found' });
+    }
+
+    const delivery = await Delivery.create(req.body);
+
+    return res.json(delivery);
   }
 
   async update(req, res) {
