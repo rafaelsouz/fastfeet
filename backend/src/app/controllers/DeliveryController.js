55b79333980640ecec48423946d525c40755fb08
Rafael Souza <raffaelsouza908@gmail.com>
2020-02-25 18:24:03 -0300

    Atualizando entregas

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -48,6 +48,12 @@ class DeliveryController {
       return res.status(401).json({ error: 'Deliveryman not found' });
     }
 
+    if (deliveryman.status === 0) {
+      return res
+        .status(401)
+        .json({ error: 'This deliveryman is disabled, activate it first' });
+    }
+
     try {
       const { id, product } = await Delivery.create(req.body);
 
@@ -64,7 +70,64 @@ class DeliveryController {
   }
 
   async update(req, res) {
-    return res.json({});
+    const schema = Yup.object().shape({
+      product: Yup.string(),
+      canceled_at: Yup.date(),
+      recipient_id: Yup.number(),
+      deliveryman_id: Yup.number(),
+      status: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
+    const user = await User.findByPk(req.userId);
+
+    if (user.admin === 0) {
+      return res.status(401).json({ error: 'You are not allowed to do this' });
+    }
+
+    const delivery = await Delivery.findByPk(req.params.id);
+
+    const { recipient_id, deliveryman_id } = req.body;
+
+    if (recipient_id && recipient_id !== delivery.recipient_id) {
+      const recipient = await Recipient.findOne({
+        where: { id: recipient_id },
+      });
+
+      if (!recipient) {
+        return res.status(401).json({ error: 'Recipient not found' });
+      }
+    }
+
+    if (deliveryman_id && deliveryman_id !== delivery.deliveryman_id) {
+      const deliveryman = await Deliveryman.findOne({
+        where: { id: deliveryman_id },
+      });
+
+      if (!deliveryman) {
+        return res.status(401).json({ error: 'Deliveryman not found' });
+      }
+
+      if (deliveryman.status === 0) {
+        return res
+          .status(401)
+          .json({ error: 'This deliveryman is disabled, activate it first' });
+      }
+    }
+
+    await delivery.update(req.body);
+
+    const { product, canceled_at } = delivery;
+
+    return res.json({
+      recipient_id,
+      deliveryman_id,
+      product,
+      canceled_at,
+    });
   }
 
   async delete(req, res) {
