faad7f38b15bf4d3c455362cde5da5fd2483cdb0
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-01 14:45:43 -0300

    Verificando se o usuario está ativo

--- backend/src/app/middlewares/auth.js
+++ backend/src/app/middlewares/auth.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
+import User from '../models/User';
 
 export default async (req, res, next) => {
   const authHead = req.headers.authorization;
@@ -16,6 +17,12 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
+
+    const user = await User.findByPk(req.userId);
+
+    if (user.status === 0) {
+      return res.status(401).json({ error: 'This user is disabled' });
+    }
 
     return next();
   } catch (error) {
