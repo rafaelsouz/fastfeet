b274b468af9426b67afb5d49a2fccd2ba0e1fb61
Rafael Souza <raffaelsouza908@gmail.com>
2020-03-08 14:31:26 -0300

    adicionando o checkAdminMiddleware e separando as rotas dos supervisores e dos administradores

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,6 +12,7 @@ import DeliveryStatusController from './app/controllers/DeliveryStatusController
 import DeliveryProblemController from './app/controllers/DeliveryProblemController';
 
 import authMiddleware from './app/middlewares/auth';
+import checkAdminMiddleware from './app/middlewares/checkAdmin'
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,15 +20,29 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-// Rotas para entregador
+// Rotas para entregadores.
 routes.get('/deliveryman/:id/deliveries', DeliveryStatusController.index);
 routes.put('/deliveryman/:id/deliveries', DeliveryStatusController.update);
+
 routes.post('/delivery/:id/problems', DeliveryProblemController.store);
+// Fim das rotas para entregadores
 
 routes.use(authMiddleware);
 
-// Rotas para a distribuidora
+// Rotas para usuários supervisores(admin === 0).
 routes.put('/users', UserController.update);
+
+routes.get('/deliverymans', DeliverymanController.index);
+
+routes.get('/delivery', DeliveryController.index);
+
+routes.get('/delivery/problems', DeliveryProblemController.index);
+routes.get('/delivery/:id/problems', DeliveryProblemController.index);
+// Fim das rotas para usuários supervisores.
+
+routes.use(checkAdminMiddleware);
+
+// Rotas para usuários admnistradores.
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 
@@ -36,18 +51,15 @@ routes.put('/recipients/:id', RecipientController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
-routes.get('/deliverymans', DeliverymanController.index);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.put('/deliverymans/:id', DeliverymanController.update);
 routes.delete('/deliverymans/:id', DeliverymanController.delete);
 
-routes.get('/delivery', DeliveryController.index);
 routes.post('/delivery', DeliveryController.store);
 routes.put('/delivery/:id', DeliveryController.update);
 routes.delete('/delivery/:id', DeliveryController.delete);
 
-routes.get('/delivery/problems', DeliveryProblemController.index);
-routes.get('/delivery/:id/problems', DeliveryProblemController.index);
 routes.delete('/problem/:id/cancel-delivery', DeliveryStatusController.delete);
 
+// Fim das rotas para usuários admnistradores.
 export default routes;
